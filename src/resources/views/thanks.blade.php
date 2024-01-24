cd77b25518ec01f3662202f6532ffd9bc5c52a0e
Tanukichi-35 <yo-fu@outlook.jp>
2024-01-24 11:46:37 +0900

    add views & create index view

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Todo App</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  @yield('css')
+</head>
+
+<body>
+  <h2>お問い合わせありがとうございました</h2>
+  <button>HOME</button>
+  <p>Thank you</p>
+</body>
+
+</html>
