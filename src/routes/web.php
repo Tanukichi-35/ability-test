e4f7c68c18ed382e7a55e50a7ac548cf5859ed9c
Tanukichi-35 <yo-fu@outlook.jp>
2024-02-03 08:29:11 +0900

    未使用のファイル参照を削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExportController;
 
